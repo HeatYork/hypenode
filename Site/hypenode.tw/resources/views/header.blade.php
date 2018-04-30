e0cfb9ce2a1cfcffb8a54aaa3b6e1dd5d88fbfd5
York <jason945119@gmail.com>
2018-04-30 15:28:34 +0800

    base blade

---
+++ Site/hypenode.tw/resources/views/header.blade.php
@@ -0,0 +1,105 @@
+<div class="page-wrapper-row">
+    <div class="page-wrapper-top">
+        <!-- BEGIN HEADER -->
+        <div class="page-header">
+
+            <!-- BEGIN HEADER TOP -->
+            <div class="page-header-top">
+                <div class="container">
+
+                    <!-- BEGIN LOGO -->
+                    <div class="page-logo" onclick="javascript:location.href='http://dev-hypenode.tw/design/index.php'">
+                        <h2 class="font-green" >
+                            HypeNode
+                        </h2>
+                    </div>
+                    <!-- END LOGO -->
+
+                    <!-- BEGIN RESPONSIVE MENU TOGGLER -->
+                    <a class="menu-toggler"></a>
+                    <!-- END RESPONSIVE MENU TOGGLER -->
+
+                    <!-- BEGIN TOP NAVIGATION MENU -->
+                    <div class="top-menu">
+                        <ui class="nav nav navbar-nav pull-right">
+                            <!-- BEGIN QUICK SIDEBAR TOGGLER -->
+                            <li class="dropdown dropdown-user dropdown-dark">
+                                <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
+                                    <span class="username username-hide-mobile">Nick</span>
+                                </a>
+                                <ul class="dropdown-menu dropdown-menu-default">
+                                    <li>
+                                        <a href="">
+                                            <i class="icon-user"></i>資訊
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="">
+                                            <i class="icon-envelope-open"></i>通知
+                                            <span class="badge badge-danger">3</span>
+                                        </a>
+                                    </li>
+                                    <li class="divider"> </li>
+                                    <li>
+                                        <a href="">
+                                            <i class="icon-settings"></i>設定
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="">
+                                            <i class="icon-lock"></i>修改密碼
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="dropdown dropdown-extended quick-sidebar-toggler">
+                                <span class="sr-only"></span>
+                                <i class="icon-logout"></i>
+                            </li>
+                            <!-- END QUICK SIDEBAR TOGGLER -->
+                        </ui>
+                    </div>
+                    <!-- END TOP NAVIGATION MENU -->
+
+                </div>
+            </div>
+            <!-- END HEADER TOP -->
+
+            <!-- BEGIN HEADER MENU -->
+            <div class="page-header-menu">
+                <div class="container">
+                    <!-- BEGIN MEGA MENU -->
+                    <div class="hor-menu">
+                        <ul class="nav navbar-nav">
+
+                            <li class="menu-dropdown classic-menu-dropdown">
+                                <a href="index.php?page=dashboard">
+                                    <i class="fas fa-tachometer-alt"></i>
+                                    儀表板
+                                </a>
+                            </li>
+
+                            <li class="menu-dropdown classic-menu-dropdown">
+                                <a>
+                                    <i class="fas fa-clipboard-list"></i>
+                                    推播紀錄
+                                </a>
+                            </li>
+
+                            <li class="menu-dropdown classic-menu-dropdown">
+                                <a href="push">
+                                    <i class="fas fa-paper-plane"></i>
+                                    建立推播活動
+                                </a>
+                            </li>
+
+                        </ul>
+                    </div>
+                    <!-- END MEGA MENU -->
+                </div>
+            </div>
+            <!-- END HEADER MENU -->
+        </div>
+        <!-- END HEADER -->
+    </div>
+</div>
---
+++ Site/hypenode.tw/resources/views/master.blade.php
@@ -0,0 +1,117 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+
+    <!-- BEGIN GLOBAL PLUGINS STYLES -->
+    <link href="public/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="public/css/font-awesome.css" rel="stylesheet" type="text/css" />
+    <link href="public/css/simple-line-icons.min.css" rel="stylesheet" type="text/css" />
+    <!-- END GLOBAL PLUGINS STYLES -->
+
+    <!-- BEGIN PAGE LEVEL PLUGINS STYLES-->
+    <!-- END PAGE LEVEL PLUGINS STYLES-->
+
+    <!-- BEGIN THEME GLOBAL STYLES -->
+    <link href="public/css/components.css" rel="stylesheet" id="style_components" type="text/css" />
+    <link href="public/css/layout.min.css" rel="stylesheet" type="text/css" />
+    <link href="public/css/default.min.css" rel="stylesheet" type="text/css" id="style_color" />
+    <!-- END THEME GLOBAL STYLES -->
+
+    <!-- BEGIN THEME PAGE LEVEL STYLES -->
+
+<!-- END THEME PAGE LEVEL STYLES -->
+
+<title>HypeNode</title>
+
+</head>
+<body class="page-container-bg-solid">
+<div class ="page-wrapper">
+    <!--START HEADER START-->
+    @include('header')
+    <!--END HEADER END-->
+
+    <!--START BODY START-->
+    <div class="page-wrapper-row full-height">
+        <div class="page-wrapper-middle">
+            <!-- BEGIN CONTAINER -->
+            <div class="page-container">
+                <!-- BEGIN CONTENT -->
+                <div class="page-content-wrapper">
+                    <!-- BEGIN CONTENT BODY -->
+                    <!-- BEGIN PAGE HEAD-->
+                    <div class="page-head">
+                        <div class="container">
+                            <!-- BEGIN PAGE TITLE -->
+                            <div class="row page-title">
+                                <h1 class="col-xs-12">
+                                    <small>子標題</small>
+                                </h1>
+                            </div>
+                            <!-- END PAGE TITLE -->
+                            <!-- BEGIN PAGE TOOLBAR -->
+                            <div class="page-toolbar">
+                                <!-- TOOL BAR -->
+                            </div>
+                            <!-- END PAGE TOOLBAR -->
+                        </div>
+                    </div>
+                    <!-- END PAGE HEAD-->
+                    <!-- BEGIN PAGE CONTENT BODY -->
+                    <div class="page-content-inner">
+                        <div class="container">
+                            <!-- BEGIN PAGE CONTENT INNER -->
+                            <div class="page-content">
+                                <div class="mt-content-body">
+                                    @yield('body')
+                                </div>
+                            </div>
+                            <!-- END PAGE CONTENT INNER -->
+                        </div>
+                    </div>
+                    <!-- END PAGE CONTENT BODY -->
+                    <!-- END CONTENT BODY -->
+                </div>
+                <!-- END CONTENT -->
+            </div>
+            <!-- END CONTAINER -->
+        </div>
+    </div>
+    <!--END BODY START-->
+
+    <!--START FOOTER START-->
+    @yield('footer')
+    <!--END FOOTER END-->
+</div>
+
+
+<!--START JSON DATA START-->
+<script>
+
+</script>
+<!--END JSON DATA END-->
+
+<!-- BEGIN GLOBAL PLUGINS PLUGINS -->
+<script src="public/js/jquery.min.js" type="text/javascript"></script>
+<script src="public/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="public/js/bootstrap-switch.min.js" type="text/javascript"></script>
+<script src="public/js/fontawesome-all.min.js" type="text/javascript"></script>
+<!-- END GLOBAL PLUGINS PLUGINS -->
+
+<!-- BEGIN PAGE LEVEL PLUGINS -->
+<!-- END PAGE LEVEL PLUGINS -->
+
+<!-- BEGIN THEME GLOBAL SCRIPTS -->
+<script src="public/js/app.min.js" type="text/javascript"></script>
+<!-- END THEME GLOBAL SCRIPTS -->
+
+<!-- BEGIN PAGE LEVEL SCRIPTS -->
+
+<!-- END PAGE LEVEL SCRIPTS -->
+
+<!-- BEGIN THEME LAYOUT SCRIPTS -->
+<script src="public/js/layout.min.js" type="text/javascript"></script>
+<!-- END THEME LAYOUT SCRIPTS -->
+</body>
+</html>
