bfaaa6db90037f705feb9da33ebda1b194da8b90
York <jason945119@gmail.com>
2018-05-14 16:59:25 +0800

    fix blade folder

---
+++ Site/hypenode.tw/resources/views/base/footer.blade.php
@@ -0,0 +1,64 @@
+<div class="page-wrapper-row">
+    <div class="page-wrapper-bottom">
+        <!-- BEGIN FOOTER -->
+        <!-- BEGIN PRE-FOOTER -->
+        <div class="page-prefooter">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3 col-sm-6 col-xs-12 footer-block">
+                        <h2>關於我們</h2>
+                        <p> 所以說我們嘰哩呱啦嘰哩呱啦嘰哩呱啦嘰哩呱啦嘰哩呱啦嘰哩呱啦嘰哩呱啦嘰哩呱啦嘰哩呱啦. </p>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-12 footer-block">
+                        <h2>Follow Us On</h2>
+                        <ul class="social-icons">
+                            <li>
+                                <a href="javascript: void(0)" data-original-title="rss" class="rss"></a>
+                            </li>
+                            <li>
+                                <a href="javascript: void(0)" data-original-title="facebook" class="facebook"></a>
+                            </li>
+                            <li>
+                                <a href="javascript: void(0)" data-original-title="twitter" class="twitter"></a>
+                            </li>
+                            <li>
+                                <a href="javascript: void(0)" data-original-title="googleplus" class="googleplus"></a>
+                            </li>
+                            <li>
+                                <a href="javascript: void(0)" data-original-title="linkedin" class="linkedin"></a>
+                            </li>
+                            <li>
+                                <a href="javascript: void(0)" data-original-title="youtube" class="youtube"></a>
+                            </li>
+                            <li>
+                                <a href="javascript: void(0)" data-original-title="vimeo" class="vimeo"></a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-12 footer-block">
+                        <h2>聯絡我們</h2>
+                        <address class="margin-bottom-40"> Phone: 800 123 3456
+                            <br> Email:
+                            <a href="mailto:info@metronic.com">info@metronic.com</a>
+                        </address>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- END PRE-FOOTER -->
+        <!-- BEGIN INNER FOOTER -->
+        <div class="page-footer">
+            <div class="container">
+                <a target="_blank" href="http://keenthemes.com">
+                    HypeNode
+                </a>
+                &copy; 2018
+            </div>
+        </div>
+        <div class="scroll-to-top">
+            <i class="icon-arrow-up"></i>
+        </div>
+        <!-- END INNER FOOTER -->
+        <!-- END FOOTER -->
+    </div>
+</div>
--- Site/hypenode.tw/resources/views/createNotification.blade.php
+++ Site/hypenode.tw/resources/views/createNotification.blade.php
@@ -1,4 +1,4 @@
-@extends('master')
+@extends('base.master')
 
 @section('pageJs')
 
